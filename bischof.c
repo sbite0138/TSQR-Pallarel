3fee37710a6e371c5fbbd41b886a4fe011fd40cd
S-bite <24842740+S-bite@users.noreply.github.com>
2021-07-02 23:54:26 +0900

    impl test()

--- bischof.c
+++ bischof.c
@@ -1,9 +1,11 @@
 #include <assert.h>
 #include <cblas.h>
 #include <lapacke.h>
+#include <math.h>
 #include <omp.h> // for a timing routine.
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 void print_matrix(char *msg, int m, int n, double *a, int lda)
 {
@@ -64,27 +66,122 @@ void construct_Q_from_compact_WY(int m, int n, double *Y, double *T, int ldy, in
     free(Q);
 }
 
+double calc_Frobenius_norm(int m, int n, double *a, int lda)
+{
+    double norm = 0.0;
+    for (int i = 0; i < m; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            norm += a[j + lda * i] * a[j + lda * i];
+        }
+    }
+    return sqrt(norm);
+}
+double *gen_matrix(int m, int n, int lda)
+{
+    assert(lda >= n);
+    double *a = malloc(sizeof(double) * m * lda);
+    for (size_t i = 0; i < m; ++i)
+    {
+        for (size_t j = 0; j < n; ++j)
+        {
+            a[j + lda * i] = (double)(rand()) / RAND_MAX;
+            // printf("%ld %ld\n", i, j);
+        }
+    }
+    return a;
+}
+
+void test()
+{
+    int n = 10;
+    double *a = gen_matrix(n, n, n);
+    double *b = malloc(sizeof(double) * n * n);
+    double *v = malloc(sizeof(double) * n * n);
+    double *vt = malloc(sizeof(double) * n * n);
+
+    double *q = malloc(sizeof(double) * n * n);
+    double norm_a = calc_Frobenius_norm(n, n, a, n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            b[j + n * i] = a[j + n * i];
+            v[j + n * i] = 0.0;
+        }
+    }
+
+    print_matrix("A = ", n, n, a, n);
+
+    double *const t = calloc(n * n, sizeof(double));
+    LAPACKE_dgeqrt3(LAPACK_ROW_MAJOR, n, n, a, n, t, n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+                v[j + n * i] = 1.0;
+            else if (i > j)
+            {
+
+                v[j + n * i] = a[j + n * i];
+                a[j + n * i] = 0.0;
+            }
+        }
+    }
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, n, n, n, 1.0, v, n, t, n, 0.0, vt, n);
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, n, n, n, -1.0, vt, n, v, n, 0.0, q, n);
+    for (int i = 0; i < n; i++)
+    {
+        q[i + n * i] += 1.0;
+    }
+
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, n, n, n, 1.0, q, n, a, n, 0.0, vt, n);
+
+    // print_matrix("t= ", L, L, t, L + 1);
+    print_matrix("v=", n, n, v, n);
+    print_matrix("t=", n, n, t, n);
+    print_matrix("q=", n, n, q, n);
+    print_matrix("A = ", n, n, b, n);
+    print_matrix("qr=", n, n, vt, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            b[j + n * i] -= vt[j + n * i];
+        }
+    }
+    double norm_qr = calc_Frobenius_norm(n, n, b, n);
+    printf("error = %e\n", norm_qr / norm_a);
+    free(t);
+}
+
 // QR分解はLAPACKを使って大丈夫
 
 //https://www.hpc.nec/documents/sdk/SDK_NLC/UsersGuide/man/dsyr2k.html
 void bischof(int matrix_layout, int N, double *a, int lda)
 {
     int L = 10;
-    int nb = 2;
+    int nb = L;
+    assert(L % nb == 0);
     assert(MIN(N, L) >= nb && nb >= 1);
     int ldt = L;
     assert(ldt >= nb);
 
     assert(N % L == 0);
-    for (int k = 0; k < N / L; k++)
+    for (int k = 0; k < N / L - 1; k++)
     {
 
         double *const t = calloc(ldt * nb, sizeof(double));
-        int Nk = (N / L - k) * L;
+        int Nk = (N / L - k + 1) * L;
         printf("%d\n", ldt);
         LAPACKE_dgeqrt(LAPACK_ROW_MAJOR, Nk, L, nb, &a[k * L + lda * k * L], lda, t, ldt);
         // print_matrix("t= ", L, L, t, L + 1);
         printf("ok %p\n", t);
+
+        print_matrix("t=", nb, L, t, ldt);
         free(t);
     }
     printf("ok");
@@ -92,14 +189,17 @@ void bischof(int matrix_layout, int N, double *a, int lda)
 
 int main(void)
 {
-    size_t const m = 30;
-
     unsigned long const random_seed = 10;
+    printf("%ld\n", time(NULL));
+    sranddev(); //srand(time(NULL));だと最初のrand()の返り値が偏る cf:https://stackoverflow.com/questions/32489058/trouble-generating-random-numbers-in-c-on-a-mac-using-xcode
+    printf("%lf\n", (double)(rand()) / RAND_MAX);
+    test();
+    return 0;
+    size_t const m = 30;
 
     size_t const lda = m + 4;
 
     double *const a = malloc(sizeof(double) * m * lda);
-    srand(random_seed);
     for (size_t i = 0; i < m; ++i)
     {
         for (size_t j = 0; j < m; ++j)
