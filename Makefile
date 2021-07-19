f767297bf0a7b751798ce67b09a38c8f63ef3447
S-bite <24842740+S-bite@users.noreply.github.com>
2021-07-19 12:55:34 +0900

    change matrix layout from row to column

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ ifeq ($(UNAME), Darwin)
 CC = /opt/homebrew/bin/gcc-11  -std=c11 -Wall -Wextra -O3 -march=native -funroll-loops -fopenmp 
 else
 ifeq ($(UNAME), Linux)
-CC = gcc -std=c11 -Wall -Wextra -g -march=native -funroll-loops -fopenmp -pg
+CC = gcc -std=c11 -Wall -Wextra -g -march=native -funroll-loops -fopenmp -pg -fsanitize=address
 endif
 endif
 OPENBLAS_DIR = ./OpenBLAS-0.3.13
--- bischof.c
+++ bischof.c
@@ -20,7 +20,7 @@ void print_matrix(char *msg, int m, int n, double *a, int lda)
         printf("[");
         for (int j = 0; j < n; j++)
         {
-            printf("%12.8lf ,", a[j + lda * i]);
+            printf("%12.8lf ,", a[i + lda * j]);
         }
         printf("],");
         printf("\n");
@@ -36,7 +36,7 @@ double calc_Frobenius_norm(int m, int n, double *a, int lda)
     {
         for (int j = 0; j < n; ++j)
         {
-            norm += a[j + lda * i] * a[j + lda * i];
+            norm += a[i + lda * j] * a[i + lda * j];
         }
     }
     return sqrt(norm);
@@ -46,7 +46,7 @@ double calc_Frobenius_norm(int m, int n, double *a, int lda)
 // cf. http://www.netlib.org/lapack/explore-html/dd/d9a/group__double_g_ecomputational_gaddcf152e87deec6123a1899f6f51101e.html
 double *construct_Q(int m, int n, double *A, int lda, double *T, int ldt)
 {
-    printf("in construct Q\n");
+    // printf("in construct Q\n");
     // Q = I - VTV^t と表すことができ、Aの下三角部分からVが構築できる
     double *V = malloc(sizeof(double) * m * n);
     double *VT = malloc(sizeof(double) * m * n);
@@ -57,23 +57,23 @@ double *construct_Q(int m, int n, double *A, int lda, double *T, int ldt)
         for (int j = 0; j < n; j++)
         {
             if (i == j)
-                V[j + n * i] = 1.0;
+                V[i + m * j] = 1.0;
             else if (i > j)
             {
 
-                V[j + n * i] = A[j + lda * i];
+                V[i + m * j] = A[i + lda * j];
             }
             else
             {
-                V[j + n * i] = 0.0;
+                V[i + m * j] = 0.0;
             }
         }
     }
     // VT = V * Tを計算
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, n, n, 1.0, V, n, T, ldt, 0.0, VT, n);
+    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, m, n, n, 1.0, V, m, T, ldt, 0.0, VT, m);
 
     // Q =I - VT * V^t を計算する
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, m, m, n, -1.0, VT, n, V, n, 0.0, Q, m);
+    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasTrans, m, m, n, -1.0, VT, m, V, m, 0.0, Q, m);
     for (int i = 0; i < m; i++)
     {
         Q[i + m * i] += 1.0;
@@ -81,7 +81,7 @@ double *construct_Q(int m, int n, double *A, int lda, double *T, int ldt)
 
     free(V);
     free(VT);
-    printf("out construct Q\n");
+    // printf("out construct Q\n");
 
     return Q;
 }
@@ -91,7 +91,7 @@ void bischof(int matrix_layout, int N, double *A, int lda, double *Q)
 {
     int L = 50; //帯行列化時の幅
     int nb = L;
-    int ldt = L;
+    int ldt = nb;
     assert(MIN(N, L) >= nb && nb >= 1);
     assert(ldt >= nb);
     assert(N % L == 0);
@@ -104,48 +104,54 @@ void bischof(int matrix_layout, int N, double *A, int lda, double *Q)
     {
         for (int j = 0; j < N; j++)
         {
-            Q[j + i * N] = (i == j ? 1.0 : 0.0);
+            Q[i + j * N] = (i == j ? 1.0 : 0.0);
         }
     }
 
     for (int k = 0; k < N / L - 1; k++)
     {
-
-        double *const t = calloc(ldt * nb, sizeof(double));
+        printf("iteration %d/%d\n", k + 1, N / L - 1);
+        double *const t = calloc(ldt * L, sizeof(double));
         int Nk = N - L - k * L;
         // Aの(k+1,k)ブロック以下をQR分解する
-        LAPACKE_dgeqrt(LAPACK_ROW_MAJOR, Nk, L, L, &A[k * L + lda * (k + 1) * L], lda, t, ldt);
+        LAPACKE_dgeqrt(LAPACK_COL_MAJOR, Nk, L, L, &A[(k + 1) * L + lda * k * L], lda, t, ldt);
         // tの下三角部分を0クリア
         for (int i = 0; i < L; i++)
         {
             for (int j = 0; j < i; j++)
             {
-                t[j + i * ldt] = 0.0;
+                t[i + j * ldt] = 0.0;
             }
         }
 
         // Qを構築する
-        double *tmp = construct_Q(Nk, L, &A[k * L + lda * (k + 1) * L], lda, t, ldt);
+        double *tmp = construct_Q(Nk, L, &A[(k + 1) * L + lda * k * L], lda, t, ldt);
+        // double *tmp2 = malloc(Nk * Nk * sizeof(double));
+        // print_matrix("construct Q = ", Nk, Nk, tmp, Nk);
+        // cblas_dgemm(CblasColMajor, CblasNoTrans, CblasTrans, Nk, Nk, Nk, 1.0, tmp, Nk, tmp, Nk, 0.0, tmp2, Nk);
+        // print_matrix("QQ^T = ", Nk, Nk, tmp2, Nk);
+        // free(tmp2);
+
         // Qnext = [I O] とする
         //         [O Q]
         for (int i = 0; i < N; i++)
         {
             for (int j = 0; j < N; j++)
             {
-                if (i < L * (k + 1) || j < (L * (k + 1)))
-                    Qnext[j + i * N] = (i == j ? 1.0 : 0.0);
+                if (j < L * (k + 1) || i < (L * (k + 1)))
+                    Qnext[i + j * N] = (i == j ? 1.0 : 0.0);
                 else
-                    Qnext[j + i * N] = tmp[(j - L * (k + 1)) + (i - L * (k + 1)) * Nk];
+                    Qnext[i + j * N] = tmp[(i - L * (k + 1)) + (j - L * (k + 1)) * Nk];
             }
         }
 
         // QをQ*Qnextで更新する
-        cblas_dgemm(CblasRowMajor, CblasTrans, CblasNoTrans, N, N, N, 1.0, Qnext, N, Q, N, 0.0, Qtmp, N);
+        cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans, N, N, N, 1.0, Qnext, N, Q, N, 0.0, Qtmp, N);
         for (int i = 0; i < N; i++)
         {
             for (int j = 0; j < N; j++)
             {
-                Q[j + i * N] = Qtmp[j + i * N];
+                Q[i + j * N] = Qtmp[i + j * N];
             }
         }
         free(tmp);
@@ -158,7 +164,7 @@ void bischof(int matrix_layout, int N, double *A, int lda, double *Q)
         double *update_Q = malloc(Nk * L * sizeof(double));
 
         // Aの(k+1,k)ブロックの先頭アドレス
-        double *a_part = &A[k * L + lda * (k + 1) * L];
+        double *a_part = &A[(k + 1) * L + lda * k * L];
 
         // Vにa_partの下三角部分を代入する(LAPACKE_dgeqrtを実行しているので，a_partの下三角部分にはQのcompact-WY表現の一部が入っている)
         for (int i = 0; i < Nk; i++)
@@ -167,15 +173,15 @@ void bischof(int matrix_layout, int N, double *A, int lda, double *Q)
             {
                 if (i == j)
                 {
-                    V[j + i * L] = 1.0;
+                    V[i + j * Nk] = 1.0;
                 }
                 else if (i > j)
                 {
-                    V[j + i * L] = a_part[j + i * lda];
+                    V[i + j * Nk] = a_part[i + j * lda];
                 }
                 else
                 {
-                    V[j + i * L] = 0.0;
+                    V[i + j * Nk] = 0.0;
                 }
             }
         }
@@ -187,46 +193,42 @@ void bischof(int matrix_layout, int N, double *A, int lda, double *Q)
             {
                 if (i > j + L)
                 {
-                    A[j + i * lda] = A[i + j * lda] = 0.0;
+                    A[i + j * lda] = A[j + i * lda] = 0.0;
                 }
                 else
                 {
-                    A[i + j * lda] = A[j + i * lda];
+                    A[j + i * lda] = A[i + j * lda];
                 }
             }
         }
-
         // 山本有作先生の『キャッシュマシン向け三重対角化アルゴリズムの性能予測方式』で説明されているBischofのアルゴリズム中のPを構築する
-        cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, Nk, L, Nk, 1.0, &A[(k + 1) * L + lda * (k + 1) * L], lda, V, L, 0.0, update_tmp, Nk);
-        cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, Nk, L, L, 1.0, update_tmp, Nk, t, ldt, 0.0, update_P, L);
+        cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, Nk, L, Nk, 1.0, &A[(k + 1) * L + lda * (k + 1) * L], lda, V, Nk, 0.0, update_tmp, Nk);
+        cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, Nk, L, L, 1.0, update_tmp, Nk, t, ldt, 0.0, update_P, Nk);
 
         // betaを構築する
-        cblas_dgemm(CblasRowMajor, CblasTrans, CblasNoTrans, L, L, Nk, 0.5, V, L, update_P, L, 0.0, update_tmp, Nk);
-        cblas_dgemm(CblasRowMajor, CblasTrans, CblasNoTrans, L, L, L, 1.0, t, ldt, update_tmp, Nk, 0.0, update_beta, L);
+        cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans, L, L, Nk, 0.5, V, Nk, update_P, Nk, 0.0, update_tmp, Nk);
+        cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans, L, L, L, 1.0, t, ldt, update_tmp, Nk, 0.0, update_beta, L);
 
         // Qを構築する
         for (int i = 0; i < Nk; i++)
         {
             for (int j = 0; j < L; j++)
             {
-                update_Q[j + i * L] = update_P[j + i * L];
+                update_Q[i + j * Nk] = update_P[i + j * Nk];
             }
         }
-        cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, Nk, L, L, -1.0, V, L, update_beta, L, 1.0, update_Q, L);
+        cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, Nk, L, L, -1.0, V, Nk, update_beta, L, 1.0, update_Q, Nk);
 
         // Aをrank-2k更新する
-        cblas_dsyr2k(CblasRowMajor, CblasUpper, CblasNoTrans, Nk, L, -1.0, V, L, update_Q, L, 1.0, &A[(k + 1) * L + lda * (k + 1) * L], lda);
+        cblas_dsyr2k(CblasColMajor, CblasUpper, CblasNoTrans, Nk, L, -1.0, V, Nk, update_Q, Nk, 1.0, &A[(k + 1) * L + lda * (k + 1) * L], lda);
+
         for (int i = 0; i < N; i++)
         {
             for (int j = i; j < N; j++)
             {
-                A[i + j * lda] = A[j + i * lda];
+                A[j + i * lda] = A[i + j * lda];
             }
         }
-
-        // AをDGEMMを使って更新する
-        // cblas_dgemm(CblasRowMajor, CblasTrans, CblasNoTrans, Nk, Nk, Nk, 1.0, tmp, Nk, &a[(k + 1) * L + lda * (k + 1) * L], lda, 0.0, tmp2, Nk);
-        // cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, Nk, Nk, Nk, 1.0, tmp2, Nk, tmp, Nk, 0.0, &a[(k + 1) * L + lda * (k + 1) * L], lda);
 
         free(update_P);
         free(update_beta);
@@ -244,7 +246,7 @@ int main(void)
     unsigned long const random_seed = 100;
     srand(random_seed);
 
-    size_t const m = 3000; // 帯行列化する行列のサイズ
+    size_t const m = 1000; // 帯行列化する行列のサイズ
     size_t const lda = m + 1;
 
     double *const a = malloc(sizeof(double) * m * lda);   // 帯行列化する行列
@@ -257,7 +259,7 @@ int main(void)
     {
         for (size_t j = 0; j < m; ++j)
         {
-            a[j + lda * i] = a[i + j * lda] = (double)(rand()) / RAND_MAX;
+            a[i + j * lda] = a[j + i * lda] = (double)(rand()) / RAND_MAX;
         }
     }
 
@@ -266,7 +268,7 @@ int main(void)
     {
         for (size_t j = 0; j < m; ++j)
         {
-            b[j + lda * i] = a[j + i * lda];
+            b[i + lda * j] = a[i + j * lda];
         }
     }
 
@@ -275,23 +277,24 @@ int main(void)
     double norm_a = calc_Frobenius_norm(m, m, a, lda);
 
     //Bischofのアルゴリズム実行
-    bischof(LAPACK_ROW_MAJOR, m, a, lda, Q);
+    bischof(LAPACK_COL_MAJOR, m, a, lda, Q);
 
     //Qが直交行列かQQ^Tを計算することで確かめる
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, m, m, m, 1.0, Q, m, Q, m, 0.0, tmp, lda);
+    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasTrans, m, m, m, 1.0, Q, m, Q, m, 0.0, tmp, lda);
+
     //直交行列ならこの値が1になっているはず
     printf("norm of QQ^T = %e\n", calc_Frobenius_norm(m, m, tmp, lda) / sqrt(m));
 
     //QaQ^Tを計算することで，帯行列化したaから元の行列aを復元する
-    cblas_dgemm(CblasRowMajor, CblasTrans, CblasNoTrans, m, m, m, 1.0, Q, m, a, lda, 0.0, tmp, lda);
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, m, m, 1.0, tmp, lda, Q, m, 0.0, a, lda);
+    cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans, m, m, m, 1.0, Q, m, a, lda, 0.0, tmp, lda);
+    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, m, m, m, 1.0, tmp, lda, Q, m, 0.0, a, lda);
 
     // 復元したaと元のaとの誤差を計算する
     for (size_t i = 0; i < m; ++i)
     {
         for (size_t j = 0; j < m; ++j)
         {
-            b[j + lda * i] -= a[j + i * lda];
+            b[i + lda * j] -= a[i + j * lda];
         }
     }
     printf("norm  = %e\n", calc_Frobenius_norm(m, m, b, lda) / norm_a);
